03633bf61b92d4bf613b8e7b1d36321fa7baffea
VastiDev <vastiane1@gmail.com>
2023-12-15 22:54:42 -0300

    start project

---
+++ src/test/java/net/atlassian/vastidev/mscloudgateway/MscloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.atlassian.vastidev.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
