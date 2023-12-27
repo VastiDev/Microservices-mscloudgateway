6f6c85e8026bb723f9506c859ff98cb2e9afe66a
VastiDev <vastiane1@gmail.com>
2023-12-27 16:47:15 -0300

    finishes section 4

--- src/main/java/net/atlassian/vastidev/mscloudgateway/MscloudgatewayApplication.java
+++ src/main/java/net/atlassian/vastidev/mscloudgateway/MscloudgatewayApplication.java
@@ -23,6 +23,8 @@ public class MscloudgatewayApplication {
 		return builder
 				.routes()
 				.route(r -> r.path("/clients/**").uri("lb://msclients"))
+				.route(r -> r.path("/cards/**").uri("lb://mscards"))
+				.route(r -> r.path("/credit-evaluator/**").uri("lb://mscreditevaluator"))
 				.build();
 	}
 }
